652da44cce4cd78041597f0a71392b1d0e9c835a
mob <mob@mikeobrien.net>
2013-01-04 20:18:30 -0500

    Update the docs.

--- src/SimpleConfig/Configuration.cs
+++ src/SimpleConfig/Configuration.cs
@@ -12,8 +12,15 @@ namespace SimpleConfig
     {
         public T Load<T>(string sectionName = null)
         {
-            return Deserializer.Create(x => x.IgnoreCase()).Deserialize<T>(
-                ((Section)ConfigurationManager.GetSection(sectionName ?? typeof(T).Name.ToCamelCase())).Element);
+            sectionName = sectionName ?? typeof (T).Name.ToCamelCase();
+            var section = ConfigurationManager.GetSection(sectionName);
+            if (!(section is Section)) throw new ConfigurationErrorsException(
+                "The configuration section '" + sectionName + 
+                "' must have a section handler of type '" +
+                typeof(Section).FullName + "'.");
+            if (section == null) throw new ConfigurationErrorsException(
+                "Could not find configuration section '" + sectionName + "'.");
+            return Deserializer.Create(x => x.IgnoreCase()).Deserialize<T>(((Section)section).Element);
         }
     }
 }
