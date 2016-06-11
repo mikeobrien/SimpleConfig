535c5f00ef862923aef72876487dcdfdc604b3c7
mob <mob@mikeobrien.net>
2016-06-10 20:26:32 -0400

    Added overload for exe config path.

--- src/SimpleConfig/Configuration.cs
+++ src/SimpleConfig/Configuration.cs
@@ -14,24 +14,35 @@ namespace SimpleConfig
     {
         private readonly string _configPath;
         private readonly Lazy<Deserializer> _deserializer;
+        private readonly ExeConfigurationFileMap _exeFileMap;
 
-        public Configuration(Action<OptionsDsl> options = null, string configPath = null)
+        public Configuration(Action<OptionsDsl> options = null, string configPath = null, ExeConfigurationFileMap exeFileMap = null)
         {
+            _exeFileMap = exeFileMap;
             _configPath = configPath;
             options = options ?? (x => { });
             _deserializer = new Lazy<Deserializer>(() => Deserializer.Create(x => options(x.IncludePublicFields()
                 .Deserialization(y => y.IgnoreNameCase().IgnoreArrayItemNames().IgnoreRootName()))));
         }
-
-        public static T Load<T>(Action<OptionsDsl> options = null, string sectionName = null, string configPath = null)
+        public static T Load<T>(ExeConfigurationFileMap exeFileMap = null)
         {
-            return new Configuration(options, configPath).LoadSection<T>(sectionName);
+            return new Configuration(null, exeFileMap: exeFileMap).LoadSection<T>(null);
+        }
+
+        public static T Load<T>(Action<OptionsDsl> options, string sectionName = null, ExeConfigurationFileMap exeFileMap = null)
+        {
+            return new Configuration(options, exeFileMap: exeFileMap).LoadSection<T>(sectionName);
+        }
+
+        public static T Load<T>(string sectionName, Action<OptionsDsl> options = null, ExeConfigurationFileMap exeFileMap = null)
+        {
+            return new Configuration(options, exeFileMap: exeFileMap).LoadSection<T>(sectionName);
         }
 
         public T LoadSection<T>(string sectionName = null, Action<DeserializerOptionsDsl> options = null)
         {
             sectionName = sectionName ?? typeof(T).GetXmlTypeName() ?? typeof(T).Name.ToCamelCase();
-            var section = GetSection(sectionName, _configPath);
+            var section = GetSection(sectionName);
             if (!(section is Section)) throw new ConfigurationErrorsException(
                 "The configuration section '" + sectionName + 
                 "' must have a section handler of type '" +
@@ -41,10 +52,11 @@ namespace SimpleConfig
             return _deserializer.Value.DeserializeXml<T>(((Section)section).Element);
         }
 
-        private object GetSection(string sectionName, string configPath)
+        private object GetSection(string sectionName)
         {
-            return configPath != null ? 
-                ConfigurationManager.OpenMappedMachineConfiguration(new ConfigurationFileMap(configPath)).GetSection(sectionName) :
+            return _configPath != null || _exeFileMap != null ? 
+                ConfigurationManager.OpenMappedMachineConfiguration(_exeFileMap ?? 
+                    new ConfigurationFileMap(_configPath)).GetSection(sectionName) :
                 ConfigurationManager.GetSection(sectionName);
         }
     }
